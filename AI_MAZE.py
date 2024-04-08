bc919d1aa942e98eb0222f184e2b70941aa5960d
SOUVIK MAHANTA <66234771+souvik03-136@users.noreply.github.com>
2024-04-08 20:19:58 +0530

    Update AI_MAZE.py

--- AI_MAZE.py
+++ AI_MAZE.py
@@ -1,24 +1,31 @@
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+genai.configure(api_key=API_KEY)
 
-genai.configure(api_key="api_key")
+
 
 def generate_dnd_maze():
-    text = "Craft an engaging Dungeons and Dragons scenario featuring a 20x40 ASCII dungeon maze, where players navigate using W, A, S, and D keys." \
+    text = "Generate an engaging Dungeons and Dragons scenario in 200 words of a maze, without any breaks, where players navigate using the W, A, S, and D keys." \
            " Begin with an illustrated maze showing a designated start (S) and end (E) point. " \
            "Incorporate traps, challenges, and commands for gameplay, seeking descriptions for each scenario. " \
            "Create an immersive adventure by setting the stage with a detailed maze description, storyline, and challenges awaiting the players."
+
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(text)
     return response.text
+
 
 maze_result = generate_dnd_maze
 
 
 def play(user_input):
-    maze1 = f"The player navigates through the maze using the following commands: w (up), a (left), s (down), d (right). "\
-          f"Based on the command, describe the scenario in the maze or provide the description of the player's current position. "\
-          f"Initially, the maze description or design looks like this: {maze_result}. "\
-          f"User Input: {user_input}"
+    maze1 = f"The player navigates through the maze using the following commands: w (up), a (left), s (down), d (right). " \
+            f"Based on the command, describe the scenario in the maze or provide the description of the player's current position. " \
+            f"Initially, the maze description or design looks like this: {maze_result}. " \
+            f"User Input: {user_input}"
     model = genai.GenerativeModel("gemini-pro")
     completion = model.generate_content(maze1)
     return completion.text
