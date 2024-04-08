6468eef985af07153a065974fb1e22159281c55e
SOUVIK MAHANTA <66234771+souvik03-136@users.noreply.github.com>
2024-04-08 15:23:02 +0530

    Update AI_MAZE.py

--- AI_MAZE.py
+++ AI_MAZE.py
@@ -1,14 +1,15 @@
-import openai
+import google.generativeai as genai
 
-openai.api_key = "api_key"
+genai.configure(api_key="api_key")
 
 def generate_dnd_maze():
     text = "Develop a playable Dungeons and Dragons scenario where the player navigates a 20x40 ASCII dungeon maze using the W, A, S, D keys." \
            " Draw the maze with a designated start (S) and end (E) point. Integrate components like traps and challenges." \
            " Offer commands for gameplay and request descriptions for each new scenario. " \
            "Commence by illustrating the initial maze and setting the stage for an immersive adventure."
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": text}])
-    return completion.choices[0].message.content
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(text)
+    return response.text
 
 maze_result = generate_dnd_maze
 
@@ -18,6 +19,6 @@ def play(user_input):
           f"Based on the command, describe the scenario in the maze or provide the description of the player's current position. "\
           f"Initially, the maze description or design looks like this: {maze_result}. "\
           f"User Input: {user_input}"
-
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":maze1}])
-    return completion.choices[0].message.content
+    model = genai.GenerativeModel("gemini-pro")
+    completion = model.generate_content(maze1)
+    return completion.text
