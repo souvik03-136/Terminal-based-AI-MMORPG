975fa0109393bd81e898f0b97d09908e3af13265
SOUVIK MAHANTA <66234771+souvik03-136@users.noreply.github.com>
2024-04-26 17:24:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 import socket
 import threading
 from AI_MAZE import generate_dnd_maze, play
+from dotenv import load_dotenv
+import os
+import random
+
+
+load_dotenv()
+PORT = int(os.getenv("PORT"))
 
 def handle_client_connection(client_socket):
     while True:
@@ -8,6 +15,14 @@ def handle_client_connection(client_socket):
         if command.lower() == '/generate_maze':
             maze_scenario = generate_dnd_maze()
             client_socket.send(f"D&D Maze: {maze_scenario}".encode())
+
+        elif command.lower() == '/roll-dice':
+            rand_idx = random.randrange(len(totalclients))
+            random_num = totalclients[rand_idx]
+            plays_next = f"Player {str(random_num)[14:-1]}: plays next"
+            print(plays_next)
+            broadcast(plays_next.encode())
+
         elif command.startswith('/msg'):
             message = command.split(' ', 1)[1]
             broadcast_message = f"Player {client_socket.getpeername()}: {message}"
@@ -21,17 +36,20 @@ def broadcast(message):
     for client in clients:
         client.send(message)
 
+totalclients = []
+
 clients = []
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 8888))
+    server_socket.bind(('localhost', PORT))
     server_socket.listen(5)
 
     while True:
         client_socket, address = server_socket.accept()
         print(f"Connection from {address}")
         clients.append(client_socket)
+        totalclients.append(client_socket.getpeername())
         client_handler = threading.Thread(target=handle_client_connection, args=(client_socket,))
         client_handler.start()
 
